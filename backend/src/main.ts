db5b0f43819a702144463192218bf113d95fb18e
Diaxpegu <diaxpegu@gmail.com>
2026-08-29 20:22:50 -0400

    feat: modulo auth configurado y endpoints para empezar

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Crea la aplicación de NestJS basado en el módulo principal
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 10000);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Prueba en local
+      'https://illustrious-bienenstitch-b47d01.netlify.app' // Prueba en la web
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  // Puerto dinámico render
+  const port = process.env.PORT || 10000;
+
+  // Encender el servidor para escuchar peticiones
+  await app.listen(port);
+  console.log(`Backend corriendo en el puerto ${port}`);
 }
 bootstrap();
